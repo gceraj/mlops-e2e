7edc25f5dc175ae9e8847d43d6968b8f7379c6b8
Pakkiaraj <gceraj@gmail.com>
2025-12-19 09:29:04 -0500

    11 api code commit

--- api/main.py
+++ api/main.py
@@ -1,8 +1,16 @@
 from fastapi import FastAPI 
+import joblib
 app = FastAPI() 
+model, vectorizer = joblib.load("models/model.pkl")
+
 @app.get("/") 
 def read_root(): 
     return {"Hello": "World"}
+
+@app.post("/predict")
+def predict(text: str):
+    vec = vectorizer.transform([text])
+    return {"prediction": model.predict(vec)[0]}
 
 
 #from fastapi import FastAPI
